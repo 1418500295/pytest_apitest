2025a491d6875c3f8ca50f8cb3de63ab55d08be0
daine <daine0513@gmail.com>
2020-03-23 09:25:43 +0800

    daine update

--- cases/test_first.py
+++ cases/test_first.py
@@ -14,11 +14,11 @@ class TestFirst():
     function 表示每个函数运行前后调用，yield之前做setup操作，yield之后做teardowm操作
     class每个测试类运行前后做一次
     """
-    @pytest.fixture(scope='function',autouse=True)
-    def simple_setup(self):
-        print("测试开始")
-        yield
-        print("测试结束")
+    # @pytest.fixture(scope='function',autouse=True)
+    # def simple_setup(self):
+    #     print("测试开始")
+    #     yield
+    #     print("测试结束")
 
     def test_01(self):
         data = OperateJson.get_json("getDemo.json")
@@ -28,18 +28,18 @@ class TestFirst():
 
     def test_02(self):
         rs = requests.get(url=FileConfig.get_url("getCookies"),params='')
-        print(rs.text)
-        print(rs.cookies)
+        # print(rs.text)
+        # print(rs.cookies)
         for k,v in rs.cookies.items():
             print(k,v)
         return rs.cookies
 
     def test_03(self):
         rs = requests.get(url=FileConfig.get_url("getwithcookies"),cookies= TestFirst().test_02())
-        print(rs.text)
+        # print(rs.text)
 
 
 if __name__ == '__main__':
-    pytest.main(['-s','-q'])
+    pytest.main(['-s','q'])
     # print(os.path.dirname(os.getcwd()))
 
--- utils/runAll.py
+++ utils/runAll.py
@@ -1,9 +1,13 @@
 import pytest
 import os
+
 project_path = os.path.dirname(os.getcwd())
 target_path = project_path + '/cases/'
+"""
+pytest支持-s, -v, -q
+"""
 if __name__ == '__main__':
-    pytest.main(['-s','-q',target_path,'--alluredir','../report/data'])
+    pytest.main(['-s','-v',target_path,'--alluredir','../report/data'])
     command = 'allure generate ../report/data -o ../report/html --clean'
     os.system(command)
 
